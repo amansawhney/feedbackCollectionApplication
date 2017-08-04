974d23818c090347f4e2eaf0b55791f729aa8256
amansawhney <amansawhney@sawhneygames.com>
2017-08-03 22:38:40 -0400

    added mongoose and connected to mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 //intialize passport
 require('./services/passport');
 
 const authRoutes = require('./routes/authRoutes');
+mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use("/auth/google", authRoutes);
